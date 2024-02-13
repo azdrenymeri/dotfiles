e47ab26b3716a7b479eec518b059bedadbf7679b
Azdren <azdreny@gmail.com>
2024-02-13 06:40:00 +0100

    Fixed git conflict issue in tmux

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -14,8 +14,12 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.ruby" },
     { import = "lazyvim.plugins.extras.lang.elixir" },
+    { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
---
+++ tmux/README.md
@@ -0,0 +1,10 @@
+# Tmux configuration dotfiles
+This is a tmux configuration repository, which contains the tmux config file with tmux plugin manager.
+
+Check [Tmux repository](https://github.com/tmux/tmux) for more documentation.
+Also check [Tmux Cheat Sheet](https://tmuxcheatsheet.com/) for a comprehensive tmux usage.
+Check [Tmux Plugin Manager](https://github.com/tmux-plugins/tpm) for additional plugins.
+
+## About current config file
+Currently there is a small number of plugins which are used and also the prefix is `<C-v>`.
+Decided on this as it seems more comfortable for the hand to be pressed.
